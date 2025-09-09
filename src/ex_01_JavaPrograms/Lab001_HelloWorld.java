a70a405f8f9e14a8fa185cadf46cb2b0c5f0335e
jner07 <jayeshner0705@gmail.com>
2025-09-09 19:30:24 +0530

    Java - Code test 3rd

---
+++ src/ex_01_JavaPrograms/Lab001_HelloWorld.java
@@ -0,0 +1,10 @@
+package ex_01_JavaPrograms;
+
+public class Lab001_HelloWorld
+{
+    public static void main(String[] args) {
+        System.out.println( "1 & Hello World- My first program");
+        System.out.println("Helo");
+        System.out.println("New line code");
+    }
+}
