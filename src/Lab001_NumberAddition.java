5062b70d6daee1462db8d6a6a50ff844b98db68e
jner07 <jayeshner0705@gmail.com>
2025-09-09 21:05:12 +0530

    Number addition - Java

---
+++ src/Lab001_NumberAddition.java
@@ -0,0 +1,8 @@
+public class Lab001_NumberAddition {
+    public static void main(String[] args) {
+        int number1 = 50;
+        int number2 = 10;
+        int sum = number1 / number2;
+        System.out.println( "if we divide  " + number1 + " from " + number2 + " then we get " + sum );
+    }
+}
