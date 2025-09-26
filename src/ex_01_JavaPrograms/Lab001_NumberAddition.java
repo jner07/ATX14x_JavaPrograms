2a6991d2552d88499c897c4fada6109bc6e2b46e
jner07 <jayeshner0705@gmail.com>
2025-09-26 13:30:35 +0530

    13 sept

---
+++ src/ex_01_JavaPrograms/Lab001_NumberAddition.java
@@ -0,0 +1,11 @@
+package ex_01_JavaPrograms;
+
+public class Lab001_NumberAddition {
+    public static void main(String[] args) {
+        int age= 73;
+         age=5;
+         age=12;
+         age = age+1;
+        System.out.println(age);
+    }
+}
