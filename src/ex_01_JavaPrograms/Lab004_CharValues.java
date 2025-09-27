23103dd41c7176e96383101e73d28878238c6cdf
jner07 <jayeshner0705@gmail.com>
2025-09-27 21:28:53 +0530

    14 sept

---
+++ src/ex_01_JavaPrograms/Lab004_CharValues.java
@@ -0,0 +1,18 @@
+package ex_01_JavaPrograms;
+
+public class Lab004_CharValues {
+    public static void main(String[] args) {
+        char Alphabet= 'A';
+        System.out.println(Alphabet);
+
+        // \n adds new line
+        char new_line= '\n';
+        System.out.println("jayesh"+new_line+"Ner");
+
+        //\b is like back spacing
+        char back_space= '\b';
+        System.out.println("jayesh"+back_space+ "Ner");
+
+
+    }
+}
