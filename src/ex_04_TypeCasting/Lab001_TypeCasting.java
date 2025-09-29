8b9fdab4e1116b72247ee4943184b2a639997c1a
jner07 <jayeshner0705@gmail.com>
2025-09-29 16:54:50 +0530

    14 sept-4

---
+++ src/ex_04_TypeCasting/Lab001_TypeCasting.java
@@ -0,0 +1,11 @@
+package ex_04_TypeCasting;
+
+public class Lab001_TypeCasting {
+    public static void main(String[] args) {
+        byte b = 10;
+        int a = b; // implicit -- happens automatically by JVM
+
+        //Implicit - big to small - Widening
+        //Explicit - Small to big - Narrowing
+    }
+}
