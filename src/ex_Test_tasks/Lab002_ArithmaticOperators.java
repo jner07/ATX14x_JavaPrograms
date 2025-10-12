b20bafce6d3868ae82e9d73fa23fe9683d320932
jner07 <jayeshner0705@gmail.com>
2025-10-12 14:44:24 +0530

    test task

---
+++ src/ex_Test_tasks/Lab002_ArithmaticOperators.java
@@ -0,0 +1,16 @@
+package ex_Test_tasks;
+
+public class Lab002_ArithmaticOperators {
+    static void main() {
+        int a = 15;
+        int b = 10;
+        System.out.println(a+b); //addition
+        System.out.println(a-b); //subtract
+        System.out.println(a*b); //multiply
+        System.out.println(a/b); //divide
+        System.out.println(a%b); //modulus
+//        System.out.println(a=b); //a have the value of b now =10
+//        System.out.println(a+=b);
+//        System.out.println(a-=b);
+    }
+}
