1699e86126c2cb2b471bfde794660c17228d47a9
jner07 <jayeshner0705@gmail.com>
2025-10-12 11:11:06 +0530

    test task

---
+++ src/ex_Test_tasks/Lab001_Java_variables.java
@@ -0,0 +1,15 @@
+package ex_Test_tasks;
+
+public class Lab001_Java_variables {
+    static void main() {
+        int age= 25;
+        double age1 = 3.14;
+        boolean bool = true;
+        char A= 'B';
+
+        System.out.println(age);
+        System.out.println(age1);
+        System.out.println(bool);
+        System.out.println(A);
+    }
+}
