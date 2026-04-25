6a21a8e0dc47f61599c3c9a920b82fb8a343b37f
jner07 <jayeshner0705@gmail.com>
2026-04-25 23:42:05 +0530

    updated

---
+++ src/CodingQuePractice/Lab09_Palindrome.java
@@ -0,0 +1,19 @@
+package CodingQuePractice;
+
+public class Lab09_Palindrome {
+
+    static void main() {
+    String input = "jayesh";
+    String reverse = "";
+
+    for (int i = input.length()-1; i>=0; i--){
+        reverse=  reverse+ input.charAt(i);
+    }
+    if (reverse.equalsIgnoreCase(input)){
+        System.out.println("this is palindrome");
+    }
+    else {
+        System.out.println("this is not palindrome");
+    }
+}
+     }
