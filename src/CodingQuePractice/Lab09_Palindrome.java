732216c9eabfb5d32f43ba63d80c95cf6ddbfdb6
jner07 <jayeshner0705@gmail.com>
2026-04-26 14:11:06 +0530

    updated

--- src/CodingQuePractice/Lab09_Palindrome.java
+++ src/CodingQuePractice/Lab09_Palindrome.java
@@ -3,17 +3,17 @@ package CodingQuePractice;
 public class Lab09_Palindrome {
 
     static void main() {
-    String input = "jayesh";
-    String reverse = "";
+        String input = "you";
+        String reverse = "";
 
-    for (int i = input.length()-1; i>=0; i--){
-        reverse=  reverse+ input.charAt(i);
+        for (int i = input.length()-1; i>=0; i--){
+            reverse = reverse+ input.charAt(i);
+        }
+        if (reverse.equalsIgnoreCase(input)){
+            System.out.println("this is palindrome");
+        }
+        else {
+            System.out.println("this is not palindrome");
+        }
     }
-    if (reverse.equalsIgnoreCase(input)){
-        System.out.println("this is palindrome");
-    }
-    else {
-        System.out.println("this is not palindrome");
-    }
-}
      }
