67c8bf829ea87d917cc274e001274beb5f85ab7b
jner07 <jayeshner0705@gmail.com>
2026-05-31 23:07:25 +0530

    updated

--- src/CodingQuePractice/Lab12_ReverseEachWord.java
+++ src/CodingQuePractice/Lab12_ReverseEachWord.java
@@ -5,19 +5,18 @@ import java.util.Scanner;
 public class Lab12_ReverseEachWord {
 
     public static String ReverseEachWord(String sentence) {
-        String[] words = sentence.split(""); //split the sentence into words
+        String[] words = sentence.split(" "); //split the sentence into words
         StringBuilder result = new StringBuilder();
 
         for (String word : words) {
             StringBuilder sb = new StringBuilder(word);
             result.append(sb.reverse()); //reverse each word
-            result.append(""); //add space between words
+            result.append(" "); //add space between words
         }
         return result.toString().trim(); //remove trailing spaces
-
     }
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a sentence");
         String input = sc.nextLine();
